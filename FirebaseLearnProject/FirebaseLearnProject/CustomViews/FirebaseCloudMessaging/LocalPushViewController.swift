71cdb4f0d0776bdcfceba6f39939038ad05c3845
sookim-1 <scstnghks@naver.com>
2024-01-10 15:02:21 +0900

    [Project] : FirebaseLearnProject추가

---
+++ FirebaseLearnProject/FirebaseLearnProject/CustomViews/FirebaseCloudMessaging/LocalPushViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LocalPushViewController.swift
+//  FirebaseLearnProject
+//
+//  Created by sookim on 1/10/24.
+//
+
+import UIKit
+
+class LocalPushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        self.title = "로컬 알림(Local Push)"
+    }
+
+
+}
---
+++ FirebaseLearnProject/FirebaseLearnProject/CustomViews/FirebaseCloudMessaging/ServerPushViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ServerPushViewController.swift
+//  FirebaseLearnProject
+//
+//  Created by sookim on 1/10/24.
+//
+
+import UIKit
+
+class ServerPushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        self.title = "원격 알림(APNs Push)"
+    }
+    
+
+
+}
